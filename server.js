5a1f4c55593bd9686694097a776fead0919e0c48
Jordi de Pablos <jordi.depablos@bicworld.com>
2021-05-15 18:07:12 +0200

    Project ready

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // server.js
 // where your node app starts
+require('dotenv').config();
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,7 +25,26 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api', (req, res, err) => {
+  const aDate = Date.now();
+  result = {
+    unix: aDate.getTime(),
+    utc: aDate.toUTCString()
+  }
+  res.json(result);
+});
 
+app.get('/api/:ts', (req, res, err) => {
+  let ts = req.params.ts;
+  if (/\d{5,}/.test(req.params.ts))
+    ts = parseInt(req.params.ts);
+  const aDate = new Date(ts);
+  result = {
+    unix: aDate.getTime(),
+    utc: aDate.toUTCString()
+  }
+  res.json(result);
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
